d819da34fd9ce3fea8d763036cd55f98d8f8b5e1
Icaro Assis <icaroassisfreitas@gmail.com>
2022-01-28 11:15:34 -0300

    atualiza script para inicializar a aplicação

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,9 +3,9 @@ const express = require('express')
 const routes = express()
 
 routes.post('/products', async (req, res) => {
-  const { name, price } = req.body
+  // const { name, price } = req.body
   try {
-    return res.status(200).json(name, price)
+    return res.status(200).json('ok')
   } catch (error) {
     return res.status(400).json(error.message)
   }
