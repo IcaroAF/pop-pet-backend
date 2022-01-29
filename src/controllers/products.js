3ac42c9df2462e91f3f46b90378997cb801798f4
Icaro Assis <icaroassisfreitas@gmail.com>
2022-01-29 01:24:06 -0300

    cria endpoint de compra de produtos

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,4 +1,5 @@
 const knex = require('../connection')
+const buyProductsSchema = require('../schemas/buyProductsSchema')
 
 const listAllProducts = async (req, res) => {
   try {
@@ -11,6 +12,30 @@ const listAllProducts = async (req, res) => {
   }
 }
 
+const buyProducts = async (req, res) => {
+  // const { id, quantidade } = req.body
+
+  try {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const product of req.body) {
+      const { id, quantidade } = product
+
+      // eslint-disable-next-line no-await-in-loop
+      await buyProductsSchema.validate(product)
+      // eslint-disable-next-line no-await-in-loop
+      await knex('products')
+        .where(product.id, id)
+        .decrement('amount', quantidade)
+      // eslint-disable-next-line no-console
+      console.log('removi')
+    }
+    return res.status(200).json('reduzido com sucesso')
+  } catch (error) {
+    return res.status(400).json(error.message)
+  }
+}
+
 module.exports = {
-  listAllProducts
+  listAllProducts,
+  buyProducts
 }
